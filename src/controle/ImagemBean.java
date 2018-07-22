04f33e1693baf06e8132b61b6c471c2745f4ca3d
Willian <Willian@DESKTOP-AJQUGMJ>
2018-07-22 16:47:25 -0300

    upload de arquivos 1.1

---
+++ src/controle/ImagemBean.java
@@ -0,0 +1,54 @@
+package controle;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+
+@RequestScoped
+@ManagedBean
+public class ImagemBean {
+	
+	@ManagedProperty("#{param.caminho}")
+	
+	private String caminho;
+	private StreamedContent foto;
+	public String getCaminho() {
+		return caminho;
+	}
+	public void setCaminho(String caminho) {
+		this.caminho = caminho;
+	}
+	public StreamedContent getFoto() throws IOException {
+		if (caminho == null || caminho.isEmpty()) {
+			 
+			Path path = Paths.get("C:/ImagensRevisApp/branco.png");
+			InputStream stream = Files.newInputStream(path);
+			foto = new  DefaultStreamedContent(stream);
+		}else {
+			
+			Path path = Paths.get(caminho);
+			InputStream stream = Files.newInputStream(path);
+			foto = new  DefaultStreamedContent(stream);
+		}
+		return foto;
+	}
+	public void setFoto(StreamedContent foto) {
+		this.foto = foto;
+	}
+	
+	public void testarCaminho() {
+		
+		System.out.println(caminho);
+	}
+	
+
+}
